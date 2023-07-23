c564fd3fff66f054aba2879d24afac77be85fc24
khaled <khaedmo@gmail.com>
2023-07-23 10:27:35 +0300

    animate the container

--- lib/animated_container.dart
+++ lib/animated_container.dart
@@ -1,3 +1,5 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 
 class AnimatedController extends StatefulWidget {
@@ -7,21 +9,47 @@ class AnimatedController extends StatefulWidget {
   State<AnimatedController> createState() => _AnimatedControllerState();
 }
 
-class _AnimatedControllerState extends State<AnimatedController> {
+class _AnimatedControllerState extends State<AnimatedController>
+    with SingleTickerProviderStateMixin {
+  late AnimationController animationController;
+  late Animation<double> animation;
   @override
-  void initState() {}
+  void initState() {
+    super.initState();
+    animationController =
+        AnimationController(vsync: this, duration: const Duration(seconds: 2));
+    animation =
+        Tween<double>(begin: 0, end: pi * 2).animate(animationController);
+    animationController.repeat();
+  }
 
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Container(
-        color: Colors.blue,
-        height: 100,
-        width: 100,
+    return Scaffold(
+      body: Center(
+        child: AnimatedBuilder(
+          animation: animation,
+          builder: (BuildContext context, Widget? child) {
+            return Transform(
+              alignment: Alignment.center,
+              transform: Matrix4.identity()..rotateY(animation.value),
+              child: Container(
+                height: 100,
+                width: 100,
+                child: Text("heelo"),
+                decoration: BoxDecoration(
+                    color: Colors.blue,
+                    borderRadius: BorderRadius.circular(10)),
+              ),
+            );
+          },
+        ),
       ),
     );
   }
 
   @override
-  void dispose() {}
+  void dispose() {
+    animationController.dispose();
+  }
 }
